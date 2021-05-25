dcefd2db22ac22c045e6cf6f1a5194689fbccd94
Rizgan Gerdenzerî <rizgan@gmail.com>
2021-05-25 12:45:39 +0300

    hyper-v shimx64.efi bootx64.efi

--- Clone_CentOS_VM_with_differencing_disk.ps1
+++ Clone_CentOS_VM_with_differencing_disk.ps1
@@ -1,9 +1,5 @@
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-PowerShell
-
 New-VHD -ParentPath: D:\Hyper-V\VHD\centos.vhdx -Path D:\Hyper-V\VHD\centOS.vhdx -Differencing
-
 New-VM -Name centOS -Generation 2 -MemoryStartupBytes 2GB -VHDPath D:\Hyper-V\VHD\centOS.vhdx -SwitchName Internal
-
 Set-VMFirmware -VMName centOS -EnableSecureBoot Off
-
 Start-VM centOS
